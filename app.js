d09cbbcba8d4285d192609794fc8588ed743fe4e
Omar-Adel <omaradel.2001.oa16@gmail.com>
2024-03-06 00:32:29 +0200

    add slider and products

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
-let currentSlide = 0;
+document.addEventListener('DOMContentLoaded', function() {
+  function fetchAndInsertHtml(url) {
+      fetch(url)
+          .then(response => response.text())
+          .then(data => {
+              document.querySelector('body').insertAdjacentHTML('afterbegin', data);
+          });
+  }
 
-  function showDetails(index) {
+  fetchAndInsertHtml('header.html');
+  fetchAndInsertHtml('footer.html');
+});
+
+
+function showDetails(index) {
     const productDetails = document.querySelectorAll('.product-details');
     productDetails.forEach((details, i) => {
-      if (i === index - 1) {
-        details.style.display = 'block';
-      } else {
-        details.style.display = 'none';
-      }
+        if (i === index - 1) {
+            if (details.style.display === 'none' || details.style.display === '') {
+                details.style.display = 'block';
+            } else {
+                details.style.display = 'none';
+            }
+        } else {
+            details.style.display = 'none';
+        }
     });
-  }
+}
+
+
+let currentIndex = 0;
+
+    function showSlide(index) {
+        const slider = document.querySelector('.product-slider');
+        const totalSlides = document.querySelectorAll('.product-box').length;
+        index = (index + totalSlides) % totalSlides; 
+        const translateValue = -index * 100 + 'vw';
+        slider.style.transform = `translateX(${translateValue})`;
+        currentIndex = index;
+    }
+
+    function nextSlide() {
+        showSlide(currentIndex + 1);
+    }
+
+    function prevSlide() {
+        showSlide(currentIndex - 1);
+    }
